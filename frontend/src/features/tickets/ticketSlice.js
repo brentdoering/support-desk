ef5a8df3c1a88d131c578409778d7ea50e3aacfa
Brent Doering <brentiscoding@gmail.com>
2023-06-30 12:50:43 -0600

    import slice into redux tree

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,3 +10,17 @@ const initialState = {
     isLoading: false,
     message: ''
 }
+
+export const ticketSlice = createSlice({
+    name: 'ticket',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+
+    }
+})
+
+export const {reset} = ticketSlice.actions
+export default ticketSlice.reducer
