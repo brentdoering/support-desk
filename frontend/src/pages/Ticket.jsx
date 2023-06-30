e43163183a2101bf3d417dd732c55be13ac545ce
Brent Doering <brentiscoding@gmail.com>
2023-06-30 14:28:36 -0600

    create closeTicket button and functionality

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import {toast} from 'react-toastify'
-import {getTicket, reset} from '../features/tickets/ticketSlice'
+import {getTicket, reset, closeTicket} from '../features/tickets/ticketSlice'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 
@@ -12,6 +12,7 @@ function Ticket() {
 
   const params = useParams()
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const {ticketId} = useParams()
 
   useEffect(() => {
@@ -22,6 +23,14 @@ function Ticket() {
     dispatch(getTicket(ticketId))
     // eslint-disable-next-line
   }, [isError, message, ticketId])
+
+
+  //Close Ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId))
+    toast.success('Ticket closed')
+    navigate('/tickets')
+  }
 
   if(isLoading) {
     return <Spinner />
@@ -48,6 +57,10 @@ function Ticket() {
       <p>{ticket.description}</p>
       </div>  
     </header>  
+
+    {ticket.status !== 'closed' && (
+      <button onClick={onTicketClose} className="btn btn-block btn-danger">Close Ticket</button>
+    )}
     </div>
   )
 }
